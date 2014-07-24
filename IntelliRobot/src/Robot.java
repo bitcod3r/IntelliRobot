457ca6c7e56109d5872750c0358c8a6214045bf8
iXi <iXi@BoxCoder>
2014-07-23 20:14:00 -0500

    Completado el movimiento diagonal del objeto.

--- IntelliRobot/src/Robot.java
+++ IntelliRobot/src/Robot.java
@@ -1,15 +1,25 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Robot {
 	
-	private int x;
-	private int y;
+	private int x = 0;
+	private int y = 100;
 	
-	private int xVelocity;
-	private int yVelocity;
+	private int xVelocity = 5;
+	private int yVelocity = 5;
 	
 	public void update() {
 		x = x + xVelocity;
 		y = y + yVelocity;
+	}
+	
+	public void paint(Graphics g) {
+		
+		g.setColor(Color.BLUE);
+		g.fillOval(x, y, 15, 15);
+		
 	}
 
 }
